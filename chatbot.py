6bcdc12bf5e93773fee7eef39b198d39ab4f3a56
Vydehi-susarla <158502197+Vydehi-susarla@users.noreply.github.com>
2025-02-28 10:08:27 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -14,14 +14,14 @@ if "messages" not in st.session_state:
     st.session_state.messages=[]
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
-        st.markdowm(message["content"])
+        st.markdown(message["content"])
 if prompt := st.chat_input("Say something..."):
-    st.session_state.message.append({"role":"user","content":prompt})
+    st.session_state.messages.append({"role":"user","content":prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
     response=st.session_state.chat.send_message(prompt)
     st.session_state.messages.append({"role":"assistant","content":response.text})
-    with st.chat_messages("assistant"):
+    with st.chat_message("assistant"):
         st.markdown(response.text)
 
 
